4354a6d5eded584407d8287b661e5cf72bf799dc
ian-moran <ian@community-technology.com>
2022-07-28 11:28:41 -0400

    OCG publications app

---
+++ web/ocg-publications/precache-manifest.0eed17d065522c8980e7b26ad653bc77.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "480afd7c1af0c4c27279",
+    "url": "/static/css/main.be72c490.css"
+  },
+  {
+    "revision": "480afd7c1af0c4c27279",
+    "url": "/static/js/main.480afd7c.js"
+  },
+  {
+    "revision": "a068c84960762fe7f57d",
+    "url": "/static/js/1.a068c849.chunk.js"
+  },
+  {
+    "revision": "8eaf3898af75e905ce1f234119e3de07",
+    "url": "/index.html"
+  }
+];
